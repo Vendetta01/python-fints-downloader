2bfb41c4c982f7aa054fa2b1c33593626f83ee7a
npodewitz <nils.podewitz@googlemail.com>
2021-01-09 14:38:53 +0100

    Update admin command added
    Added functionality except the actual post request to update the data.

--- src/fints_downloader/management/commands/update_data.py
+++ src/fints_downloader/management/commands/update_data.py
@@ -1,6 +1,6 @@
-from django.core.management.base import BaseCommand #, CommandError
-from fints_downloader.services import FinTSService
-from fints_downloader.models import Bank, BankLogin
+from django.core.management.base import BaseCommand, CommandError
+from fints_downloader.models import BankLogin, Account
+
 
 class Command(BaseCommand):
     """Management command to update data"""
@@ -8,44 +8,71 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         # Positional arguments
-        #parser.add_argument('poll_ids', nargs='+', type=int)
+        # parser.add_argument('iban', nargs='+', type=string, help='')
 
         # Named (optional) arguments
         parser.add_argument(
+            '-i',
             '--interactive',
             action='store_true',
             help='Update in interactive mode',
         )
+        parser.add_argument(
+            '-b',
+            '--iban',
+            help='Update this iban only'
+        )
+        parser.add_argument(
+            '-n',
+            '--account_number',
+            type=int,
+            help='Update this account number only'
+        )
 
     def handle(self, *args, **options):
-        for lc in BankLogin.objects.all():
-            f = FinTSService(lc, options['interactive'])
-            accounts = f.get_accounts()
-            for account in accounts:
-                # First create or update account
-                account.save()
-                
-                # Now get balance
-                balance = f.get_balance(account)
-                balance.save()
+        interactive = options.get('interactive')
+        iban = options.get('iban')
+        account_number = options.get('account_number')
 
-                # Get transactions
-                transactions = f.get_transactions(account)
-                for transaction in transactions:
-                    # Create foreign accounts if necessary
-                    # TODO
-                    transaction.save()
+        if iban:
+            # Update this iban only
+            account = Account.objects.filter(iban=iban).first()
+            if not account:
+                raise CommandError(f"IBAN '{iban}' does not exist!")
+            elif not account.bank_login:
+                raise CommandError(f"IBAN '{iban}' has no login credentials!")
+            self._update_account(account, interactive)
+        elif account_number:
+            # Update this account number only
+            account = Account.objects.filter(number=account_number).first()
+            if not account:
+                raise CommandError((f"Account number '{account_number}' "
+                    "does not exist!"))
+            elif not account.bank_login:
+                raise CommandError((f"Account number '{account_number}' "
+                    "has no login credentials!"))
+            self._update_account(account, interactive)
+        else:
+            # Update all accounts
+            for bank_login in BankLogin.objects.all():
+                for account in Account.objects.filter(bank_login=bank_login):
+                    self._update_account(account, interactive)
 
-            self.stdout.write(self.style.SUCCESS('Successfully updated "%s"' % lc))
+    def _update_account(self, account, interactive):
+        if not account.bank_login.enabled:
+            self.stdout.write(self.style.WARNING(
+                f"Account '{account}' is disabled, ignoring..."))
+            return
+        elif account.bank_login.tan_required:
+            self.stdout.write(self.style.WARNING(
+                f"Account '{account}' requires user interaction, ignoring..."))
+            return
 
-
-#        for poll_id in options['poll_ids']:
-#            try:
-#                poll = Poll.objects.get(pk=poll_id)
-#            except Poll.DoesNotExist:
-#                raise CommandError('Poll "%s" does not exist' % poll_id)
-#
-#            poll.opened = False
-#            poll.save()
-#
-#            self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+        # Update account
+        raise Exception("Not implemented yet")
+        # TODO:
+        # -> extract max transaction date
+        # -> post request on import/transaction
+        # -> extract result from response (where are we redirected to?)
+        self.stdout.write(self.style.SUCCESS(
+            f"Account '{account}' successfully updated."))
